b469fb5ed000560394eeb1da3cd078be9d222687
Michael Hopkins <mhopkins321@gmail.com>
2015-06-04 15:00:58 -0600

    cleaning up script.

--- preinstallScript1.sh
+++ preinstallScript1.sh
@@ -6,4 +6,4 @@ curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | 
 echo 'export HOMEBREW_CASK_OPTS="--appdir=/Applications"' >> ~/.zshrc
 echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.zshrc
 cd ~/.oh-my-zsh/custom/plugins
-git clone git://github.com/zsh-users/zsh-syntax-highlighting.gi
+git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
